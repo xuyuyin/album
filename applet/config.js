e6b97eff1e173112f3b81ab96d02963a4574dea7
xuyuyin <windybamb@163.com>
2017-04-26 19:07:41 +0800

    first commit

---
+++ applet/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    appTitle: '相册回收站',
+
+    /** 通讯域名 */
+    host: 'www.xuyuyin.win',
+    basePath: '/applet/album',
+};
---
+++ server/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    port: '9993',
+    ROUTE_BASE_PATH: '/applet',
+
+    cosAppId: '1253543070',
+    cosSecretId: 'AKIDKFyXvI8Xw9B0c3FSZUFoEUYu69zd4MPG',
+    cosSecretKey: 'r6C6qSwELJ6i3C9yd5MQH2azzb7QYUHr',
+    cosFileBucket: 'xiangce',
+};
